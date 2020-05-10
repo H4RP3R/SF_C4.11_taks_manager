3a8dfe83ce4f994725a7f04480cc29d3d5532f64
H4RP3R <zombiehunter-vm@yandex.ru>
2020-05-10 19:35:52 +0300

    alert message functions properly

---
+++ src/components/Confirmation.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <b-alert variant="success"
+        dismissible
+        @dismissed="$emit('disable')"
+        show>{{ message }}</b-alert>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['message'],
+};
+</script>
